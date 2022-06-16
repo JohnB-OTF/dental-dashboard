31e38e7416b743cd1376e86b1fe07449eaa899be
John Botero <jhondark@gmail.com>
2022-06-15 19:37:48 -0500

    add calendar component

---
+++ Components/Calendar/Calendar.jsx
@@ -0,0 +1,27 @@
+//libraries
+import { useState } from "react"
+import DatePicker from "react-datepicker"
+import "react-datepicker/dist/react-datepicker.css"
+
+const Calendar = () => {
+  const [dateRange, setDateRange] = useState([null, null])
+  const [startDate, endDate] = dateRange
+
+  console.log("dateRef", new Date())
+
+  console.log(dateRange)
+
+  return (
+    <DatePicker
+      selectsRange={true}
+      startDate={startDate}
+      endDate={endDate}
+      onChange={(update) => {
+        setDateRange(update)
+      }}
+      isClearable={true}
+    />
+  )
+}
+
+export default Calendar
--- pages/DashBoard/index.jsx
+++ pages/DashBoard/index.jsx
@@ -1,9 +1,9 @@
 import Head from "next/head"
-import Image from "next/image"
 import styles from "./dashBoard.module.scss"
 
 //componets
 import CardInfo from "../../Components/CardInfo/CardInfo"
+import Calendar from "../../Components/Calendar/Calendar"
 
 const DashBoard = () => {
   return (
@@ -16,13 +16,19 @@ const DashBoard = () => {
 
       <main className={styles.main}>
         <section className={styles.main__section}>
+          <div className={styles.main__section__filters}>
+            <Calendar />
+          </div>
+        </section>
+
+        <section className={styles.main__section}>
           <div className={styles.main__section_titles}>
             <h2>New Leads</h2>
           </div>
           <div className={styles.main__section_cards}>
             <CardInfo title="Leads" value="100" percent="-6.00" />
             <CardInfo title="Connected" value="80" percent="4.5" />
-            <CardInfo title="Leads" value="100" percent="-6.00" />
+            <CardInfo title="Connected %" value="50%" percent="-6.00" />
           </div>
         </section>
 
@@ -31,9 +37,13 @@ const DashBoard = () => {
             <h2>Response Time</h2>
           </div>
           <div className={styles.main__section_cards}>
-            <CardInfo title="Leads" value="100" percent="-6.00" />
-            <CardInfo title="Leads" value="100" percent="-6.00" />
-            <CardInfo title="Leads" value="100" percent="-6.00" />
+            <CardInfo title="5 Min Response" value="59" percent="7" />
+            <CardInfo title="5 Min Response %" value="55%" percent="6.3" />
+            <CardInfo
+              title="Avg. Response Time"
+              value="03:12:11"
+              percent="-11"
+            />
           </div>
         </section>
 
@@ -42,9 +52,13 @@ const DashBoard = () => {
             <h2>Booking Time</h2>
           </div>
           <div className={styles.main__section_cards}>
-            <CardInfo title="Leads" value="100" percent="-6.00" />
-            <CardInfo title="Leads" value="100" percent="-6.00" />
-            <CardInfo title="Leads" value="100" percent="-6.00" />
+            <CardInfo title="Same Day Booking" value="27" percent="-1.2" />
+            <CardInfo title="Same Day Booking %" value="38%" percent="2.4" />
+            <CardInfo
+              title="avg. Time to Booking"
+              value="17:14:12"
+              percent="4.6"
+            />
           </div>
         </section>
       </main>
